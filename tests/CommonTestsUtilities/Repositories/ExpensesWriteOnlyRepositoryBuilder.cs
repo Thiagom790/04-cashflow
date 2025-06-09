f8bc3c625ccb1df6fef7a07b9411e3e22e2e75ab
Thiago Teixeira <thiagom790@gmail.com>
2025-06-09 09:33:19 -0300

    test: create test for Register Expense

---
+++ tests/CommonTestsUtilities/Repositories/ExpensesWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Domain.Repositories.Expenses;
+using Moq;
+
+namespace CommonTestsUtilities.Repositories;
+
+public class ExpensesWriteOnlyRepositoryBuilder
+{
+    public static IExpensesWriteOnlyRepository Build()
+    {
+        var mock = new Mock<IExpensesWriteOnlyRepository>();
+
+        return mock.Object;
+    }
+}
---
+++ tests/WebApi.Test/Expenses/Register/RegisterExpenseTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using CashFlow.Execption;
+using CommonTestsUtilities.Requests;
+using FluentAssertions;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Expenses.Register;
+
+public class RegisterExpenseTest(CustomWebApplicationFactory webApplicationFactory)
+    : CashFlowClassFixture(webApplicationFactory)
+{
+    private const string METHOD = "api/expenses";
+
+    private readonly string _token = webApplicationFactory.GetToken();
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        var result = await DoPost(METHOD, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        response.RootElement.GetProperty("title").GetString().Should().Be(request.Title);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Title_Empty(string culture)
+    {
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        var result = await DoPost(requestUri: METHOD, request: request, culture: culture);
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        var expectedMessage =
+            ResourceErrorMessages.ResourceManager.GetString("TITLE_REQUIRED", new CultureInfo(culture));
+
+        errors.Should().HaveCount(1).And.Contain((error) => error.GetString()!.Equals(expectedMessage));
+    }
+}
